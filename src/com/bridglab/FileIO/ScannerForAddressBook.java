b081798e40a7a21c65b0f00b23fb1c374a50aedb
unknown <avinash.sankajji94@gmai.com>
2021-11-02 21:17:03 +0530

    Welcome to Filo IO

---
+++ src/com/bridglab/FileIO/ScannerForAddressBook.java
@@ -0,0 +1,11 @@
+package com.bridglab.FileIO;
+
+
+import java.util.Scanner;
+
+public class ScannerForAddressBook {
+    public Scanner scannerProvider(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner;
+    }
+}
